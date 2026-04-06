eef96778c14ea8603cf47670aa3447c50a2fbeaf
Sean Kim <sean.kim428@gmail.com>
2026-04-06 11:01:00 +0900

    2주차 객체지향 자바코드

---
+++ 2주차/김건호/Lion.java
@@ -0,0 +1,10 @@
+public class Lion {
+    String name;
+    String depart;
+    int cn;
+    public Lion(String name, String depart, int cn) {
+        this.name = name;
+        this.depart = depart;
+        this.cn = cn;
+    }
+}
